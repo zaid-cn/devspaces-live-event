e09292708a070c796d0a04123a9bb66dfcbd574a
dbroemme <darren.broemmer@gmail.com>
2020-12-18 21:26:34 +0000

    Added Java setup

---
+++ src/main/java/com/devfactory/App.java
@@ -0,0 +1,10 @@
+package com.devfactory;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println("Hello World!");
+        System.out.println(" ");
+    }
+}
